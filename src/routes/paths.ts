666a71ac56bc596370cc5b156dc8fba9b669eccb
letientai <letai97md@gmail.com>
2026-09-11 23:35:28 +0700

    Implement UI for Locations Canberrra page

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -38,6 +38,9 @@ export const PATHS = {
   educationPackaging: `${EDUCATION}/packaging-requirements`,
   educationAccountEnquiries: `${EDUCATION}/account-enquiries`,
   educationTracking: `${EDUCATION}/tracking`,
+
+  // Locations city pages
+  locationsCanberra: `${LOCATIONS}/canberra`,
 } as const;
 
 export type NavItem = {
