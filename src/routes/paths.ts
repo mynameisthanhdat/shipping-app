e11eab527d4f82d2caf40e84f3aca981b1d9fa63
letientai <letai97md@gmail.com>
2026-09-11 22:51:32 +0700

    Implement UI for Locations Sunshine Coast page

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -38,6 +38,9 @@ export const PATHS = {
   educationPackaging: `${EDUCATION}/packaging-requirements`,
   educationAccountEnquiries: `${EDUCATION}/account-enquiries`,
   educationTracking: `${EDUCATION}/tracking`,
+
+  // Locations city pages
+  locationsSunshineCoast: `${LOCATIONS}/sunshine-coast`,
 } as const;
 
 export type NavItem = {
